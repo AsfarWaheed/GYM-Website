15dcad8116a9cf808d656ba7db76be68e5dba969
Asfar Waheed <114741996+AsfarWaheed@users.noreply.github.com>
2023-05-28 18:45:13 +0500

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,3 +1,15 @@
 <?php
+// connection.php
+// Database configuration
+$host = 'localhost'; // Replace with your database host
+$username = 'root'; // Replace with your database username
+$password = ''; // Replace with your database password
+$database = 'gym website'; // Replace with your database name
 
-?>
+// Create a database connection
+$conn = new mysqli($host, $username, $password, $database);
+
+// Check if the connection was successful
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
